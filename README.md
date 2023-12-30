895b28b5a0f806f5b6dbfabdc43655a945a88b33
Ellis Judge <git@ellisjudge.com>
2023-12-30 17:11:30 +0000

    First implementation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Simple Password Filter
+
+Install on all domain controllers to prevent users settings passwords that include specific strings. This is useful to prevent common passwords like the company name or passwords that include "password".
+
+## Compilation
+
+* Make a folder called build 
+* Switch into it in the msbuild tools command prompt and run the below.
+
+```
+cmake .. -G "Visual Studio 17 2022" -A x64
+msbuild PasswordFilter.sln /p:Configuration=Release /p:Platform=x64
+```
+
+## Installation
+* Copy the DLL from the build/Release/x64 folder to C:\Windows\System32
+* Update registry key `HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa` Add `PasswordFilter` to the REG_MULTI_SZ `Notification Packages` 
+* Create a file `C:\Windows\PasswordFilter.txt` with forbidden words listed in lowercase 1 per line
+* Restart server
+
+See the [Microsoft documentation](https://learn.microsoft.com/en-us/windows/win32/secmgmt/installing-and-registering-a-password-filter-dll) on installing password filters for more details
+
+## Compatibility
+This was tested on a Windows Server 2022 domain controller. Likely it would work with older versions of Windows Server if an older Windows SDK is installed.
---
+++ library.cpp
@@ -0,0 +1,81 @@
+#define _WIN32_WINNT _WIN32_WINNT_WIN10
+#define WINVER _WIN32_WINNT_WIN10
+
+#include <Windows.h>
+#include <iostream>
+#include <winternl.h>
+#include <ntstatus.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#define MAX_ITERATIONS 10000
+
+extern "C" __declspec(dllexport) BOOLEAN __stdcall
+PasswordFilter(PUNICODE_STRING AccountName, PUNICODE_STRING FullName, PUNICODE_STRING Password, BOOLEAN SetOperation);
+
+extern "C" __declspec(dllexport) BOOLEAN __stdcall InitializeChangeNotify();
+
+extern "C" __declspec(dllexport) NTSTATUS __stdcall PasswordChangeNotify(PUNICODE_STRING UserName, ULONG RelativeId, PUNICODE_STRING NewPassword);
+
+bool StringContainsSubstring(PUNICODE_STRING str, PUNICODE_STRING substr);
+
+void CloneString(PUNICODE_STRING src, PUNICODE_STRING dst);
+
+void DeleteString(PUNICODE_STRING string);
+
+std::vector<PUNICODE_STRING> forbiddenWords;
+
+extern "C" __declspec(dllexport) BOOLEAN __stdcall InitializeChangeNotify() {
+
+    std::wifstream file(L"C:\\Windows\\PasswordFilter.txt");
+    std::wstring line;
+    int index = 0;
+
+    while (std::getline(file, line) && index < MAX_ITERATIONS) {
+        PUNICODE_STRING string = new UNICODE_STRING;
+        string->Length = line.length();
+        string->MaximumLength = string->Length + 1;
+        string->Buffer = new wchar_t[string->MaximumLength + 1];
+        string->Buffer[string->MaximumLength] = L'\0';
+        wcsncpy_s(string->Buffer, string->MaximumLength + 1, line.c_str(), line.length());
+        forbiddenWords.push_back(string);
+    }
+
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) BOOLEAN __stdcall
+PasswordFilter(PUNICODE_STRING AccountName, PUNICODE_STRING FullName, PUNICODE_STRING Password, BOOLEAN SetOperation) {
+    UNICODE_STRING lowercase;
+    CloneString(Password, &lowercase);
+    _wcslwr_s(lowercase.Buffer, lowercase.Length);
+    bool match = false;
+    for (auto &forbiddenWord: forbiddenWords) {
+        if (StringContainsSubstring(&lowercase, forbiddenWord)) {
+            match = true;
+        }
+    }
+    DeleteString(&lowercase);
+    return match ? FALSE : TRUE;
+}
+
+extern "C" __declspec(dllexport) NTSTATUS __stdcall PasswordChangeNotify(PUNICODE_STRING UserName, ULONG RelativeId, PUNICODE_STRING NewPassword) {
+    return STATUS_SUCCESS;
+}
+
+bool StringContainsSubstring(PUNICODE_STRING str, PUNICODE_STRING substr) {
+    return wcsstr(str->Buffer, substr->Buffer) != nullptr;
+}
+
+void CloneString(PUNICODE_STRING src, PUNICODE_STRING dst) {
+    auto *buffer = new wchar_t[src->Length + 1];
+    wcsncpy_s(buffer, src->Length, src->Buffer, src->Length);
+    buffer[src->Length] = '\0';
+    RtlInitUnicodeString(dst, buffer);
+}
+
+void DeleteString(PUNICODE_STRING string) {
+    SecureZeroMemory(string->Buffer, string->Length);
+    delete[] string->Buffer;
+}
